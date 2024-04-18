6970a9cb27fd12abc4f1b01e72138630b54d376f
saku <pg.saku0719@gmail.com>
2024-04-18 15:27:02 +0900

    [add] lesson05

--- src/pages/Lesson05/index.js
+++ src/pages/Lesson05/index.js
@@ -4,7 +4,22 @@ import Chart from "../../components/Chart05";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  const maxVal = Math.max(...input.map((v) => Math.round(v.y)));
+  const minVal = Math.min(...input.map((v) => Math.round(v.y)));
+  const classLength = Array.from({ length: maxVal - minVal + 1 }, (_, index) =>
+    String(index + minVal)
+  );
+  return classLength.map((bin) => {
+    return {
+      bin: bin,
+      男性: input.filter(
+        (n) => String(Math.round(n.y)) === bin && n.gender === "男性"
+      ).length,
+      女性: input.filter(
+        (n) => String(Math.round(n.y)) === bin && n.gender === "女性"
+      ).length,
+    };
+  });
 };
 
 const Lesson = () => {
