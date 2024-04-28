440f84fde2cee85309f3c5e405ff84745b3a85b7
saku <pg.saku0719@gmail.com>
2024-04-28 23:01:00 +0900

    [add] lesson06

--- src/pages/Lesson06/index.js
+++ src/pages/Lesson06/index.js
@@ -4,7 +4,15 @@ import Chart from "../../components/Chart06";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  return input.map((v) => {
+    return {
+      color: v.gender === "男性" ? "blue" : "red",
+      gender: v.gender,
+      bmi: v.x / Math.pow(v.y / 100, 2),
+      weight: v.x,
+      height: v.y,
+    };
+  });
 };
 
 const Lesson = () => {
