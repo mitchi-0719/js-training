4a945e3799977728ab1dd205108ed7947003f3d0
saku <pg.saku0719@gmail.com>
2024-04-29 00:40:57 +0900

    [add] lesson7

--- src/pages/Lesson07/index.js
+++ src/pages/Lesson07/index.js
@@ -4,7 +4,49 @@ import Chart from "../../components/Chart07";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  const utcToJst = (d) => {
+    const utcDate = new Date(d);
+    const diff = 9 * 60 * 60 * 1000;
+    const jstDate = new Date(utcDate.getTime() + diff);
+    return jstDate.toLocaleDateString("ja-JP", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    }).replaceAll("/", "-");
+  };
+
+  const date = Array.from(
+    new Set(
+      input.map(({ createdAt }) => {
+        return utcToJst(createdAt);
+      })
+    )
+  );
+  return [
+    {
+      id: "tweet",
+      data: date.map((d) => {
+        return {
+          x: d,
+          y: input.filter(
+            ({ createdAt, isRetweet }) =>
+              utcToJst(createdAt) === d && !isRetweet
+          ).length,
+        };
+      }),
+    },
+    {
+      id: "reTweet",
+      data: date.map((d) => {
+        return {
+          x: d,
+          y: input.filter(
+            ({ createdAt, isRetweet }) => utcToJst(createdAt) === d && isRetweet
+          ).length,
+        };
+      }),
+    },
+  ];
 };
 
 const Lesson = () => {
