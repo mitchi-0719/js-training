1fc8b4c7e7397ae6637d9c56bf7a88ee87f74e92
saku <pg.saku0719@gmail.com>
2024-05-04 19:08:48 +0900

    [add] lesson8

--- src/pages/Lesson08/index.js
+++ src/pages/Lesson08/index.js
@@ -4,7 +4,49 @@ import Chart from "../../components/Chart08";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  return { nodes: [], links: [] }; // ここを作りましょう！
+  const tagsList = input.map(({ tags }) => {
+    return tags;
+  });
+
+  const allLinks = tagsList
+    .map((tags) => {
+      return tags
+        .map((tag, i) => {
+          const _tags = tags.slice(i + 1);
+          return _tags.map((_tag) => {
+            return [tag, _tag];
+          });
+        })
+        .flat();
+    })
+    .flat();
+
+  const links = Array.from(
+    new Set(
+      allLinks
+        .map((link) => {
+          if (
+            allLinks.filter(
+              (_link) => JSON.stringify(link) === JSON.stringify(_link)
+            ).length >= 2
+          ) {
+            const sort_link = link.sort();
+            return { source: sort_link[0], target: sort_link[1] };
+          }
+        })
+        .filter((v) => v !== undefined)
+        .map(JSON.stringify)
+    ),
+    JSON.parse
+  );
+
+  const nodes = Array.from(new Set(links.flatMap((v) => Object.values(v)))).map(
+    (v) => {
+      return { id: v };
+    }
+  );
+
+  return { nodes: nodes, links: links }; // ここを作りましょう！
 };
 
 const Lesson = () => {
